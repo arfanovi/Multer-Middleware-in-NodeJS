f1cb65926885a81d600430ab858d1247e5653493
arfanovi <arfanovi.gub1@gmail.com>
2024-08-28 22:38:21 +0600

    Using multer method and adding get

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const PORT = 3001;
 const app = express();
 
 
+// This is multer middleware with the destination directory file upload 
+const upload = multer({dest: 'uploads/'});
+
+
+app.get('/', (req, res) =>{
+    res.send(`
+        
+        <h2>File Upload</h2>
+        <form action='/upload' method="POST" enctype="multipart/form-data">
+        <input type = 'file' name ="file"/>
+        <button type="submit">Upload</button>
+        
+        </form>
+        `)
+})
